5e34ffcf91372f01515ac778f992b67a3a8fd8a7
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-07-03 16:31:07 -0700

    router setup
    - Setup export index.js for pages directory for convenience
    - Added react router setup to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Footer, Navbar, Sidebar } from "./components";
+import {
+  AboutPage,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  PrivateRoute,
+  ProductsPage,
+  SingleProductPage,
+} from "./pages";
+import { project } from "./ProjectProperties";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route path={project.nav.homepage} exact>
+          <HomePage />
+        </Route>
+        <Route path={project.nav.products} exact>
+          <ProductsPage />
+        </Route>
+        <Route path={project.nav.about} exact>
+          <AboutPage />
+        </Route>
+        <Route path={project.nav.cart} exact>
+          <CartPage />
+        </Route>
+        <Route path={project.nav.checkout} exact>
+          <CheckoutPage />
+        </Route>
+        <Route
+          path={project.nav.singleProduct}
+          children={<SingleProductPage />}
+          exact
+        />
+        <Route path={project.nav.error}>
+          <ErrorPage />
+        </Route>
+        <PrivateRoute>
+          <HomePage />
+        </PrivateRoute>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/ProjectProperties.js
@@ -0,0 +1,15 @@
+const project = {
+  name: "React-Comfy_Sloth",
+  nav: {
+    homepage: "/React-Comfy_Sloth",
+    products: "/React-Comfy_Sloth/products",
+    singleProduct: "/React-Comfy_Sloth/products/:id",
+    about: "/React-Comfy_Sloth/about",
+    cart: "/React-Comfy_Sloth/cart",
+    checkout: "/React-Comfy_Sloth/checkout",
+    login: "/React-Comfy_Sloth/login",
+    error: "*",
+  },
+};
+
+export { project };

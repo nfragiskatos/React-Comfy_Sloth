5ae53c470a956deb7bee3dbd97e2eaf92002f122
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-07-04 07:36:16 -0700

    Hero impl for homepage

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import heroBcg from '../assets/hero-bcg.jpeg'
-import heroBcg2 from '../assets/hero-bcg-2.jpeg'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import heroBcg2 from "../assets/hero-bcg-2.jpeg";
+import heroBcg from "../assets/hero-bcg.jpeg";
+import { project } from "../ProjectProperties";
 
 const Hero = () => {
-  return <h4>hero</h4>
-}
+  return (
+    <Wrapper className="section-center">
+      <article className="content">
+        <h1>
+          design your <br />
+          comfort zone
+        </h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis
+          excepturi, commodi, non, accusamus quaerat rerum dolorem voluptatem
+          nemo consectetur quae ab? Vel totam ullam rerum, sequi vitae
+          voluptatibus cumque vero dignissimos sit architecto voluptatem sed
+          doloribus quo libero
+        </p>
+        <Link to={project.nav.products} className="btn hero-btn">
+          shop now
+        </Link>
+      </article>
+      <article className="img-container">
+        <img src={heroBcg} alt="nice table" className="main-img" />
+        <img src={heroBcg2} alt="person working" className="accent-img" />
+      </article>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   min-height: 60vh;
@@ -58,7 +82,7 @@ const Wrapper = styled.section`
       border-radius: var(--radius);
     }
     .img-container::before {
-      content: '';
+      content: "";
       position: absolute;
       width: 10%;
       height: 80%;
@@ -68,6 +92,6 @@ const Wrapper = styled.section`
       border-radius: var(--radius);
     }
   }
-`
+`;
 
-export default Hero
+export default Hero;

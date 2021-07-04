7918c6d55bedbf6e5b8e7b985759b05751d8b39a
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-07-04 08:41:58 -0700

    fetching products from API
    - Handled fetching all products from the API in  the products context, and updating the state via the products reducer

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,9 +1,21 @@
+import axios from "axios";
 import React, { useContext, useEffect, useReducer } from "react";
-import { SIDEBAR_CLOSE, SIDEBAR_OPEN } from "../actions";
+import {
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_ERROR,
+  GET_PRODUCTS_SUCCESS,
+  SIDEBAR_CLOSE,
+  SIDEBAR_OPEN,
+} from "../actions";
 import reducer from "../reducers/products_reducer";
+import { products_url as url } from "../utils/constants";
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -19,8 +31,19 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
 
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const resp = await axios.get(url);
+      const products = resp.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
+  };
+
   useEffect(() => {
-    // openSidebar();
+    fetchProducts(url);
   }, []);
 
   return (
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,4 +1,10 @@
-import { SIDEBAR_CLOSE, SIDEBAR_OPEN } from "../actions";
+import {
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_ERROR,
+  GET_PRODUCTS_SUCCESS,
+  SIDEBAR_CLOSE,
+  SIDEBAR_OPEN,
+} from "../actions";
 
 const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_OPEN) {
@@ -6,6 +12,24 @@ const products_reducer = (state, action) => {
   }
   if (action.type === SIDEBAR_CLOSE) {
     state = { ...state, isSidebarOpen: false };
+  }
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    );
+
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
   }
   return state;
   throw new Error(`No Matching "${action.type}" - action type`);

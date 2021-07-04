fa62f5bd336f081d7ddb21f195044112bad993bf
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-07-04 07:27:47 -0700

    initial implementation of about page, checkout page, and hero
    - each given a hero to display at top
    - Hero takes in title prop

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import aboutImg from "../assets/hero-bcg.jpeg";
+import { PageHero } from "../components";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+            provident ipsum dolores veritatis blanditiis alias dolorum suscipit,
+            dicta quasi culpa expedita? Ad impedit expedita perferendis pariatur
+            rerum consequatur, repellendus aliquam. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Odit sint impedit cum porro possimus,
+            ea eius culpa quis cumque dolorem. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Quod vitae quos ullam harum modi non
+            dicta corporis suscipit, at autem aut, deleniti animi ipsam eum
+            aliquam eveniet? Nesciunt, beatae incidunt?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +57,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;

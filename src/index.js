8ec4c2043a3003c58b583484606039462b7001bb
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-07-05 12:07:42 -0700

    Added Auth0 for account creation/authorization handling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
@@ -5,15 +6,23 @@ import { CartProvider } from "./context/cart_context";
 import { FilterProvider } from "./context/filter_context";
 import { ProductsProvider } from "./context/products_context";
 import "./index.css";
+import { project } from "./ProjectProperties";
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirectUri={`${window.location.origin}${project.nav.homepage}`}
+    cacheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
 
   document.getElementById("root")
 );

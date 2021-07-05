275a3db98bcb3fc5d35ba67e1cc430ac95149306
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-07-05 06:49:05 -0700

    List view for products page

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,9 +4,19 @@ import GridView from "./GridView";
 import ListView from "./ListView";
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext();
-  <ListView />;
-  return <GridView products={products}></GridView>;
+  const { filtered_products: products, grid_view } = useFilterContext();
+
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: "none" }}>
+        Sorry, no products matched your criteria
+      </h5>
+    );
+  }
+  if (grid_view) {
+    return <GridView products={products} />;
+  }
+  return <ListView products={products} />;
 };
 
 export default ProductList;

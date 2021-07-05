51b3b3b98629f17dd3548a64ef3478feab0421ff
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-07-05 08:47:41 -0700

    impl for company filter

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -58,6 +58,24 @@ const Filters = () => {
               })}
             </div>
           </div>
+          <div className="form-control">
+            <h5>company</h5>
+            <select
+              name="company"
+              id="company"
+              value={company}
+              onChange={updateFilters}
+              className="company"
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
         </form>
       </div>
     </Wrapper>
